f52d48e432c6447f9f80e501ae0f301618cfde47
Matti Sangi <matti.sangi.ext@talenom.fi>
2019-04-01 17:09:21 +0300

    Solution proposal

--- src/components/greeting.ts
+++ src/components/greeting.ts
@@ -1,3 +1,27 @@
 ﻿export class Greeting {
     private message = "Hello, world!";
+    selectedFiles!:FileList;
+
+    inputChange() {
+        let image = new Image();
+        
+        image.onload = function () {
+            let elem = this as HTMLImageElement;
+
+            let canvas = <HTMLCanvasElement> document.createElement('canvas');
+
+            canvas.width = elem.naturalWidth;
+            canvas.height = elem.naturalHeight;
+
+            let context = <CanvasRenderingContext2D> canvas.getContext('2d');
+
+            context.drawImage(elem, 0, 0);
+
+            let stringImg = canvas.toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, '');
+        };
+
+        let file = this.selectedFiles.item(0);
+        if (file !== null)
+            image.src = URL.createObjectURL(file);
+    }
 }
