443a53fb787635daffac0456d83a1e7d236db6ea
Matti Sangi <matti.sangi.ext@talenom.fi>
2019-04-02 16:56:28 +0300

    Solution updated

--- src/components/greeting.ts
+++ src/components/greeting.ts
@@ -1,10 +1,13 @@
 ﻿export class Greeting {
     private message = "Hello, world!";
+    private stringImg: string = '';
+    private list: any;
     selectedFiles!:FileList;
 
     inputChange() {
         let image = new Image();
-        
+
+        let _this = this;
         image.onload = function () {
             let elem = this as HTMLImageElement;
 
@@ -17,11 +20,22 @@
 
             context.drawImage(elem, 0, 0);
 
-            let stringImg = canvas.toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, '');
+            _this.stringImg = canvas.toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, '');
         };
 
         let file = this.selectedFiles.item(0);
         if (file !== null)
             image.src = URL.createObjectURL(file);
+    }
+
+    submit() {
+        this.list = {
+            "a_Rows": [
+                {
+                    "pkiCustomerID": "1",
+                    "simage": this.stringImg,
+                },
+            ]
+        }
     }
 }
