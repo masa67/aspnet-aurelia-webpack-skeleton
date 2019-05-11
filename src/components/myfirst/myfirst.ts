67d670b907413f359608f8e738e21f529f939034
Matti Sangi <adm.masaweb@gmail.com>
2019-05-11 12:11:42 +0300

    Test basic parameter passing

--- src/components/myfirst/myfirst.ts
+++ src/components/myfirst/myfirst.ts
@@ -1,6 +1,15 @@
 ﻿export class MyFirst {
     private firstName = 'John';
     private surName = 'Doe';
+    private myTitle = 'green';
+
+    constructor() {
+        var self = this;
+        setTimeout(function () {
+            self.myTitle = 'red';
+            alert('changing to red...');
+        }, 3000);
+    }
 
     sayHello() {
         alert(`Hello, ${this.firstName} ${this.surName}!`);
