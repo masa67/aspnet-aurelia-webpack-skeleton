cb03abcfc7aaf61a35ab48f3451838da76440a6f
Matti Sangi <matti.sangi.ext@talenom.fi>
2019-05-29 13:43:20 +0300

    Querying related data: add simple test for one-to-one mapping

--- UnitTest/UnitTest3.cs
+++ UnitTest/UnitTest3.cs
@@ -14,7 +14,14 @@ namespace UnitTest
     {
         public string Name { get; set; }
 
+        public Address Address { get; set; }
+
         public List<ContactPerson> ContactPersons { get; set; }
+    }
+
+    public class Address
+    {
+        public string City { get; set; }
     }
 
     public class ContactPerson
@@ -30,8 +37,8 @@ namespace UnitTest
         {
             testObjects = new List<Customer>()
             {
-                new Customer { Name = "Company1" },
-                new Customer { Name = "Company2" }
+                new Customer { Name = "Company1", Address = new Address() { City = "London" } },
+                new Customer { Name = "Company2", Address = new Address() { City = "New York" } }
             };
 
             var contactPersons = new List<ContactPerson>();
@@ -96,7 +103,47 @@ namespace UnitTest
             Assert.Pass();
         }
 
+        [Test]
+        // Test one-to-one mapping
         public void Test2()
+        {
+            InitTestObjects();
+
+            IQueryable<Customer> queryableTestObjects = testObjects.AsQueryable();
+
+            var queryString = "[ { \"property\":\"Address.Name\", \"operator\":\"Contains\", \"value\":\"London\" } ]";
+
+            var query = QueryHelper.GetQuery(queryString);
+
+            var param1 = Expression.Parameter(typeof(Customer), "o");
+            var property1 = Expression.Property(param1, "Address");
+
+            var call1 = Expression.Call(
+                Expression.Property( property1, "City"),
+                typeof(String).GetMethod("IndexOf", new Type[] { typeof(String), typeof(StringComparison) }),
+                    new Expression[] {
+                        Expression.Constant("London"),
+                        Expression.Constant(StringComparison.OrdinalIgnoreCase)
+                    }
+            );
+
+            var condition = Expression.GreaterThanOrEqual(call1, Expression.Constant(0));
+
+            Expression<Func<Customer, bool>> predicate = Expression.Lambda<Func<Customer, bool>>(condition, param1);
+            //var predicate = QueryHelper.GenerateWhere<Customer>(query.Query);
+
+            var result = queryableTestObjects.Where(predicate).ToList();
+
+            // The above is the same as this:
+            // var result = queryableTestObjects.Where(o => o.Address.City.Contains("London")).ToList();
+            
+            Assert.IsTrue(result.Count == 1);
+            Assert.IsTrue(result[0].Name == "Company1");
+
+            Assert.Pass();
+        }
+
+        public void Test33()
         {
             InitTestObjects();
 
