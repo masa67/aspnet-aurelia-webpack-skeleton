b6f5f2336dc91cf07bfd73bdc84b1c6c189c51b2
Matti Sangi <matti.sangi.ext@talenom.fi>
2019-05-29 12:29:20 +0300

    Querying related data: refactor simple test to use dynamic linq

--- UnitTest/UnitTest3.cs
+++ UnitTest/UnitTest3.cs
@@ -2,6 +2,7 @@
 using Client.Query;
 using NUnit.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -53,14 +54,41 @@ namespace UnitTest
 
             var query = QueryHelper.GetQuery(queryString);
 
+            var param1 = Expression.Parameter(typeof(Customer), "o");
+            var property1 = Expression.Property(param1, "ContactPersons");
 
+            var listItemType = property1.Type.GetProperty("Item").PropertyType;
+            var entityProperty = listItemType.GetProperty("Name");
 
+            var param2 = Expression.Parameter(listItemType, "p");
 
+            var call1 = Expression.Call(
+                Expression.Property(param2, entityProperty),
+                typeof(String).GetMethod("IndexOf", new Type[] { typeof(String), typeof(StringComparison) }),
+                    new Expression[] {
+                        Expression.Constant("Jane"),
+                        Expression.Constant(StringComparison.OrdinalIgnoreCase)
+                    }
+            );
 
+            var equality = Expression.GreaterThanOrEqual(call1, Expression.Constant(0));
 
+            
+            var anyCall = Expression.Call(
+                typeof(Enumerable),
+                nameof(Enumerable.Any),
+                new Type[] { param2.Type },
+                property1,
+                Expression.Lambda(equality, param2)
+            );
+
+            Expression<Func<Customer, bool>> predicate = Expression.Lambda<Func<Customer, bool>>(anyCall, param1);
             //var predicate = QueryHelper.GenerateWhere<Customer>(query.Query);
 
-            var result = queryableTestObjects.Where(o => o.ContactPersons.Any(p => p.Name.Contains("Jane"))).ToList();
+            var result = queryableTestObjects.Where(predicate).ToList();
+
+            // The above is the same as this:
+            // var result = queryableTestObjects.Where(o => o.ContactPersons.Any(p => p.Name.Contains("Jane"))).ToList();
 
             Assert.IsTrue(result.Count == 1);
             Assert.IsTrue(result[0].Name == "Company1");
