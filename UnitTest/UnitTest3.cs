240423e59bf3b3101ef2069186b7739c06ffdc0c
Matti Sangi <matti.sangi.ext@talenom.fi>
2019-05-29 13:45:57 +0300

    Querying related data: switch Test1 and Test2

--- UnitTest/UnitTest3.cs
+++ UnitTest/UnitTest3.cs
@@ -51,8 +51,48 @@ namespace UnitTest
         }
 
         [Test]
-        // Test one-to-many mapping
+        // Test one-to-one mapping
         public void Test1()
+        {
+            InitTestObjects();
+
+            IQueryable<Customer> queryableTestObjects = testObjects.AsQueryable();
+
+            var queryString = "[ { \"property\":\"Address.Name\", \"operator\":\"Contains\", \"value\":\"London\" } ]";
+
+            var query = QueryHelper.GetQuery(queryString);
+
+            var param1 = Expression.Parameter(typeof(Customer), "o");
+            var property1 = Expression.Property(param1, "Address");
+
+            var call1 = Expression.Call(
+                Expression.Property(property1, "City"),
+                typeof(String).GetMethod("IndexOf", new Type[] { typeof(String), typeof(StringComparison) }),
+                    new Expression[] {
+                        Expression.Constant("London"),
+                        Expression.Constant(StringComparison.OrdinalIgnoreCase)
+                    }
+            );
+
+            var condition = Expression.GreaterThanOrEqual(call1, Expression.Constant(0));
+
+            Expression<Func<Customer, bool>> predicate = Expression.Lambda<Func<Customer, bool>>(condition, param1);
+            //var predicate = QueryHelper.GenerateWhere<Customer>(query.Query);
+
+            var result = queryableTestObjects.Where(predicate).ToList();
+
+            // The above is the same as this:
+            // var result = queryableTestObjects.Where(o => o.Address.City.Contains("London")).ToList();
+
+            Assert.IsTrue(result.Count == 1);
+            Assert.IsTrue(result[0].Name == "Company1");
+
+            Assert.Pass();
+        }
+
+        [Test]
+        // Test one-to-many mapping
+        public void Test2()
         {
             InitTestObjects();
 
@@ -97,46 +137,6 @@ namespace UnitTest
             // The above is the same as this:
             // var result = queryableTestObjects.Where(o => o.ContactPersons.Any(p => p.Name.Contains("Jane"))).ToList();
 
-            Assert.IsTrue(result.Count == 1);
-            Assert.IsTrue(result[0].Name == "Company1");
-
-            Assert.Pass();
-        }
-
-        [Test]
-        // Test one-to-one mapping
-        public void Test2()
-        {
-            InitTestObjects();
-
-            IQueryable<Customer> queryableTestObjects = testObjects.AsQueryable();
-
-            var queryString = "[ { \"property\":\"Address.Name\", \"operator\":\"Contains\", \"value\":\"London\" } ]";
-
-            var query = QueryHelper.GetQuery(queryString);
-
-            var param1 = Expression.Parameter(typeof(Customer), "o");
-            var property1 = Expression.Property(param1, "Address");
-
-            var call1 = Expression.Call(
-                Expression.Property( property1, "City"),
-                typeof(String).GetMethod("IndexOf", new Type[] { typeof(String), typeof(StringComparison) }),
-                    new Expression[] {
-                        Expression.Constant("London"),
-                        Expression.Constant(StringComparison.OrdinalIgnoreCase)
-                    }
-            );
-
-            var condition = Expression.GreaterThanOrEqual(call1, Expression.Constant(0));
-
-            Expression<Func<Customer, bool>> predicate = Expression.Lambda<Func<Customer, bool>>(condition, param1);
-            //var predicate = QueryHelper.GenerateWhere<Customer>(query.Query);
-
-            var result = queryableTestObjects.Where(predicate).ToList();
-
-            // The above is the same as this:
-            // var result = queryableTestObjects.Where(o => o.Address.City.Contains("London")).ToList();
-            
             Assert.IsTrue(result.Count == 1);
             Assert.IsTrue(result[0].Name == "Company1");
 
